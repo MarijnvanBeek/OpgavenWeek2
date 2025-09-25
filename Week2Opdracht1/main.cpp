088fa0e8a8fbd3dc744c8f6c568cc4bd60cf9d7a
Marijn Beek, van <s1215221@student.windesheim.nl>
2025-09-25 20:52:27 +0200

    Opgave 1

--- Week2Opdracht1/main.cpp
+++ Week2Opdracht1/main.cpp
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
 int main() {
+	int getal1;
+	int getal2;
 
+	printf("Geef een getal");
+	scanf_s("%d", &getal1);
+
+
+	printf("Geef nog een getal");
+	scanf_s("%d", &getal2);
+
+	int getal3 = getal1 * getal2
+		if (getal3 > 214783647); {
+			printf("Overflow");
+		}
+		else printf("Het getal is %d", getal3);
 }
